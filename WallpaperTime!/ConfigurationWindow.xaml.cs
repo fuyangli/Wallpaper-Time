82d411c5a752401c989262948ed0cf83592ec7e3
Gabriel Fernandes Lopes Guaitolini <guaitolinigabriel@gmail.com>
2015-04-01 21:58:58 -0300

    new configuration window

---
+++ WallpaperTime!/ConfigurationWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using WallpaperTime_.Annotations;
+using Xceed.Wpf.Toolkit;
+using Button = System.Windows.Controls.Button;
+using ComboBox = System.Windows.Controls.ComboBox;
+using DataFormats = System.Windows.DataFormats;
+using DragEventArgs = System.Windows.DragEventArgs;
+using TextBox = System.Windows.Controls.TextBox;
+
+namespace WallpaperTime_
+{
+    /// <summary>
+    /// Interaction logic for ConfigurationWindow.xaml
+    /// </summary>
+    public partial class ConfigurationWindow : INotifyPropertyChanged
+    {
+        private WallpaperTrigger _wallpaperTrigger;
+        private readonly OpenFileDialog _fileDialog = new OpenFileDialog();
+
+        public WallpaperTrigger WallpaperTrigger {
+            get { return _wallpaperTrigger; }
+            set {
+                _wallpaperTrigger = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ConfigurationWindow(WallpaperTrigger item) {
+            InitializeComponent();
+            WallpaperTrigger = item;
+            Title = string.Format("{0} @ {1}", item.Name, item.Time.ToShortTimeString());
+            
+        }
+
+        private void ButtonUrlPicker(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+            var codecs = String.Join(";", ImageCodecInfo.GetImageEncoders().Select(c => c.FilenameExtension).ToList());
+            _fileDialog.Filter = String.Format("{0} ({1}) | {1}", Properties.Resources.ImageFiles, codecs);
+            if (_fileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            TextBoxImagePath.Text = _fileDialog.FileName;
+            //CanSave = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ButtonSaveOnClick(object sender, RoutedEventArgs e) {
+            TextBoxImagePath.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            TimePicker.GetBindingExpression(TimePicker.ValueProperty).UpdateSource();
+            ComboBoxStyle.GetBindingExpression(ComboBox.SelectedValueProperty).UpdateSource();
+            Close();
+        }
+
+        private void ButtonOpenImageClick(object sender, RoutedEventArgs e) {
+            Process.Start(new ProcessStartInfo() {
+                FileName = TextBoxImagePath.Text
+            });
+        }
+
+        private void ButtonOpenContainingFolder(object sender, RoutedEventArgs e) {
+            var path = Path.GetDirectoryName(TextBoxImagePath.Text);
+            if (String.IsNullOrEmpty(path)) {
+                return;
+            }
+            if (TextBoxImagePath.Text != null) {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = path
+                });
+            }
+        }
+
+        private void ImageOnDrop(object sender, DragEventArgs e) {
+            var s = (sender as Image);
+            var data = e.Data.GetData(DataFormats.FileDrop) as String[];
+            TextBoxImagePath.Text = data[0];
+        }
+    }
+}
